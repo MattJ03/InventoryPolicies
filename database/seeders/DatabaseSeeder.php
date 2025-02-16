6674014aa89aa15b12f3032c1b234314966bf0a5
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-02-16 14:51:20 +0000

    setting up seeder with roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use App\Models\Product;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+    $adminRole = Role::create(['name' => 'admin']);
+    $userRole = Role::create(['name' => 'user']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    Permission::create(['name' => 'create product']);
+    Permission::create(['name' => 'edit product', ])
+
+    $adminRole->givePermissionTo(['create product', 'edit product', 'delete product', 'update product', 'view product']);
+    $userRole->givePermissionTo(['create product', 'view product']);
+
+    $user = User::create([
+        'name' => 'Matt',
+        'email' => 'matt@gmail.com',
+        'password' => bcrypt('password'),
+    ]);
+    $user->assignRole('admin');
+
+    $admin = User::create([
+        'name' => 'admin',
+        'email' => 'admin@admin.com',
+        'password', bcrypt('password')
+    ]);
+    $admin->assignRole('admin');
+
+
+    $category = Category::create([
+        'name' => 'Electronics'
+    ]);
+
+    $product = Product::create([
+        'name' => 'computer',
+        'quantity' => 100,
+        'price' => 1500,
+        'category_id' => $category->id,
+        'user_id' => $userRole->id,
+    ]);
     }
 }
