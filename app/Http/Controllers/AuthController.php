76dbd1f0fe549e74cd7d92acaa24c7918a819a42
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-02-16 22:06:39 +0000

    authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validatedData = $request->validate([
+            'email' => 'required|unique:users|max:255',
+        ]);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+     Auth::create($validatedData) ;
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+        }
+        else {
+            abort(401);
+        }
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+    }
+
+
+
+}
